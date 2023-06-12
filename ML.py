5a4a2062b17b72c9ce75f9b7602e8cb5a2e7e4a9
JulissaMC <127567902+JulissaMC@users.noreply.github.com>
2023-06-12 18:38:07 -0500

    Add files via upload

--- ML.py
+++ ML.py
@@ -1,3 +1,34 @@
 import pandas as pd
-df = pd.read_csv("cancer.csv")
+df = pd.read_csv("cancer.csv",index_col="id")
 print(df)
+
+filas = df.head()
+print("\n Se mostrará solo las primeras 5 filas \n", filas)
+
+filas_diez = df.head(10)
+print("\n Se mostrará solo las primeras 10 filas \n",filas_diez)
+
+ult_filas = df.tail(8)
+print("\n Se mostrará solo las últimas 8 filas \n", ult_filas)
+
+estadistica = df.describe()
+print("Se mostrará una nueva matriz con las estadísitcas descriptivas de cada columna", estadistica)
+
+#dropna quita las filas con valores nulos, creo que quitó todas las filas xd
+filtrado = df.dropna()
+print("\n Matriz limpia \n", filtrado)
+
+#cambio de valores de nulos a 0
+cambio = df.fillna(0)
+print("\n Valores nulos cambiados por 0 \n", cambio)
+
+"""también se pueden cambiar valores por columnas
+y se haría mediante un diccionario: 
+df.fillna({"radius_mean":0, "perimeter_mean":1})"""
+
+#selecciono los datos de una sola columna
+col = df["radius_mean"]
+print("\n Accedo solo a los datos de esa columna \n", col)
+
+#filtro por condiciones, en este caso que el radio sea mayor que 27u
+print("\n Filtro por \n", df[df["radius_mean"]>27])
