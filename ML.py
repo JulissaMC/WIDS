bd2aabe3f3219dc77ff5d7b11f22145887f9e2c7
Alexito Snow <116590887+AlexitoSnow@users.noreply.github.com>
2023-06-14 17:26:34 +0000

    Manipulación de representación gráfica  y Entrenamiento y Pruebas

--- ML.py
+++ ML.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 df = pd.read_csv("cancer.csv",index_col="id")
 print(df)
 
@@ -38,8 +40,9 @@ print("\n Filtro por \n", df[df["radius_mean"]>27])
 print("Conteo de los tipos de cáncer")
 grupo = df["diagnosis"].groupby(df["diagnosis"]).count()
 print(grupo)
-grupo.plot(kind='pie')
+df["diagnosis"].value_counts().plot.pie(autopct='%1.1f%%', colors = ["lightgreen","pink"],labels=["Benignos","Malignos"],title="Tipos de Cáncer")
 plt.show()
+plt.savefig("tipos_cancer.png")
 
 #Máximo valores
 print("Máximo valores")
@@ -50,3 +53,16 @@ print(maxi)
 print("Mínimo valores")
 mini = df.groupby(df["diagnosis"]).min()
 print(mini)
+ 
+y = df["diagnosis"]
+#selecciona todas las columnas menos la de diagnosis
+x = df.drop(columns=["diagnosis"])
+print(y)
+print(x)
+print('\n'*5)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+print(x_train,'\n'*3)
+print(x_test,'\n'*3)
+print(y_train,'\n'*3)
+print(y_test,'\n'*3)
